1a60f1ceddd843b43c3eda96c69f0b78d12eece4
victorchu8 <victorchu1996@gmail.com>
2018-01-24 19:41:23 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by VICTOR CHU on 2018-01-24.
+//  Copyright © 2018 Victor Chu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
